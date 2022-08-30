20457cda6d76b80e082176b128881790a644ac5b
studmanj <bjackso3@hotmail.com>
2022-08-30 13:32:00 -0600

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./header"
-import Main from "./main"
+import Header from "./Header"
+import Main from "./Main"
 
 function App() {
   return (
     <>
       <Header />
-      
+      <Main />
     </>
   );
 }
