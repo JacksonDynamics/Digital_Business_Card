33bd1f2ab730762665f9243f7beb6a2f9c6853b6
studmanj <bjackso3@hotmail.com>
2022-09-02 14:04:45 -0700

    About and Interest sections

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,19 @@ function Main() {
                 <h3 className="main--title">Frontend Developer</h3>
                 <a href="https://jacksondynamics.github.io/MyPortfolio/">jacksondynamics.MyPortfolio</a>
             </div>
+            <button id="email">Email</button>
+            <button id="linkedIn">LinkedIn</button>
+            <div id="primary--info">
+                <div className="about">
+                    <h3>About</h3>
+                    <h4 className="main--about">Experienced professional with comprehensive understanding in key industry programming languages and Frontend web development. I have a particular interest in making things simple when automating daily tasks. In additon to my IT skills, I also possess strong backgrounds from years of working in both architectural and financial industry. I consider myself industrious and teachable with a proven track record for quickly grasping concepts, and am always looking for new things to learn.
+                    </h4>
+                </div>
+                <div className="interests">
+                    <h3>Interests</h3>
+                    <h4 className="main--interests">A little more about me as a person. I have lived abroad in the Philippines for 2 years, and speak Tagalog fluently. I am an Eagle Scout, swing dance instructor, artist, and have played the guitar for over 20 years. I enjoy traveling, camping, hiking, swimming, racquetball, astronomy, aeronautics, history and am basically the epitome of nerd when it comes to board games, movies and cultural trivia. My goal in life is to become the best I can be and continuously work to improve myself in every area that I dedicate my time and energy to!</h4>
+                </div>
+            </div>
 
         </main>
     )
