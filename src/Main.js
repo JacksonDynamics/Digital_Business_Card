c2258bedc6567a66b2f94eb1150684cce0980c22
studmanj <bjackso3@hotmail.com>
2022-09-02 12:41:02 -0700

    Main Title

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,13 @@
 function Main() {
     return(
-        <h1>Main</h1>
+        <main id="main--container">
+            <div className="main--header">
+                <h1 className="main--name">Brendon Jackson</h1>
+                <h3 className="main--title">Frontend Developer</h3>
+                <a href="https://jacksondynamics.github.io/MyPortfolio/">jacksondynamics.MyPortfolio</a>
+            </div>
+
+        </main>
     )
 }
 
